a8c0b234bc6005bbd07e4f04eb6e7bf82c739d4e
Douglas Fils <dfils@oceanleadership.org>
2016-08-14 15:34:45 -0500

    Stubbed out more of the indexing code.

--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"bytes"
+	"crypto/md5"
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
 	"path/filepath"
+
+	uuid "github.com/twinj/uuid"
 )
+
+// func contentText(data []byte) string {
+
+// 	// POST data to tika and get back the text
+
+// }
 
 func visit(path string, f os.FileInfo, err error) error {
 
@@ -17,15 +29,48 @@ func visit(path string, f os.FileInfo, err error) error {
 
 	fmt.Printf("Visited file: %s\n", path)
 
+	//m5
+	data, _ := ioutil.ReadFile(path)
+	fmt.Printf("MD5:  %x", md5.Sum(data))
+
+	//uuid
+	u4 := uuid.NewV4()
+	fmt.Println(u4)
+	fmt.Printf("UUID: version %d variant %x: %s\n", u4.Version(), u4.Variant(), u4)
+
+	//content POST
+	// req, err := sling.New().Post("http://upload.com/gophers")
+	url := "http://localhost:9998/tika"
+
+	// filter out some files that we don't want to index?
+	// filter out stop words and numbers?  No need to index these.
+	req, err := http.NewRequest("PUT", url, bytes.NewBuffer(data))
+	// req.Header.Set("X-Custom-Header", "myvalue")
+	req.Header.Set("Accept", "text/plain")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("response Status:", resp.Status)
+	fmt.Println("response Headers:", resp.Header)
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println("response Body:", string(body))
+
 	//  NEED the following SEVEN functions to be written...  all should be relatively easy...
 	//  Calls to Tike will be REST client calls.
 	//  Call to triple store a simple HTTP call with a teplated SPARQL query
-	// filemeta := GetFileMeta(path string, f os.FileInfo)  // Tika call
+	// filemeta := GetFileMeta(path string, f os.FileInfo)  // Tika call  (do we need this?)
 	// contentText := GetFileContent()  // Tike call
 	// projMeta := GetProjectMeta()  // SPARQL call
 	// md5value := GetMD5Value()    // function call
 	// fileUUID := GenerateUUID()   // function call
 
+	// data := []byte("These pretzels are making me thirsty.")
+	//
 	// All the above should be in a struct..  from that JSON and RDF can be built.
 
 	// indexJSON()  // pass to Bleve for indexing
@@ -37,6 +82,7 @@ func visit(path string, f os.FileInfo, err error) error {
 func main() {
 	flag.Parse()
 	root := flag.Arg(0)
+
 	err := filepath.Walk(root, visit)
 	fmt.Printf("filepath.Walk() returned %v\n", err)
 }
