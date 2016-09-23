8fd7b36ce0290bf9a43832dbf08e46780e25b6a4
Douglas Fils <dfils@oceanleadership.org>
2016-09-23 17:28:12 -0500

    Added in the beleve indexing..  RDF left to do

--- Indexer/main.go
+++ Indexer/main.go
@@ -12,6 +12,7 @@ import (
 	"unicode/utf8"
 
 	"github.com/bbalet/stopwords"
+	"github.com/blevesearch/bleve"
 	uuid "github.com/twinj/uuid"
 )
 
@@ -29,12 +30,83 @@ func main() {
 	flag.Parse()
 	root := flag.Arg(0)
 
-	err := filepath.Walk(root, visit)
-	fmt.Printf("filepath.Walk() returned %v\n", err)
+	// err := filepath.Walk(root, visit)
+	// fmt.Printf("filepath.Walk() returned %v\n", err)
+
+	size, err := dirSize(root)
+	fmt.Printf("dirSize returned %d %v\n", size, err)
+}
+
+func dirSize(path string) (int64, error) {
+	// open file system for triples
+
+	// open a new Bleve index
+	mapping := bleve.NewIndexMapping()
+	// analyzer := mapping.Ad
+	index, berr := bleve.New("csdcoFX.bleve", mapping)
+	if berr != nil {
+		fmt.Printf("Bleve error making index %v \n", berr)
+	}
+
+	// TODO Create the triple store set
+
+	var size int64
+	err := filepath.Walk(path, func(_ string, f os.FileInfo, err error) error {
+		if !f.IsDir() {
+			size += f.Size()
+			fileInfo := FileMetadata{}
+
+			// Not sure why when doing a closure I need to rebuild the path name...
+			FQP := fmt.Sprintf("%s/%s", path, f.Name())
+
+			// md5
+			data, _ := ioutil.ReadFile(FQP)
+			fileInfo.MD5 = md5.Sum(data)
+
+			// uuid
+			u4 := uuid.NewV4()
+			fileInfo.UUID = u4.String()
+
+			// content via Tika
+			url := "http://localhost:9998/tika"
+			req, err := http.NewRequest("PUT", url, bytes.NewBuffer(data))
+			req.Header.Set("Accept", "text/plain")
+
+			client := &http.Client{}
+			resp, err := client.Do(req)
+			if err != nil {
+				fmt.Println(err)
+			}
+			defer resp.Body.Close()
+
+			// filter out some files that we don't want to index? dot files, what else?
+			fmt.Println("Response Status:", resp.Status)
+			body, _ := ioutil.ReadAll(resp.Body)
+			cleanBody := stopwords.CleanString(string(body), "en", true)
+			fileInfo.LenContentNoStopWords = utf8.RuneCountInString(cleanBody)
+			dir, file := filepath.Split(FQP)
+
+			berr = index.Index(fileInfo.UUID, fileInfo)
+			if berr != nil {
+				fmt.Printf("Bleve error indexing %v \n", berr)
+			}
+
+			// TODO Build the triples here and then append to the master set
+
+			// // split so I can use a slice element in a lookup for metadata
+			// fmt.Printf("%q\n", strings.Split(path, "/"))
+
+			fmt.Printf("For path:\t%s \nFor dir:\t%s\nFor file:\t%s\nMD5:\t%v \nUUID:\t%s \nContentLen:\t%v \n\n", FQP, dir, file, fileInfo.MD5, fileInfo.UUID, fileInfo.LenContentNoStopWords)
+		}
+		return err
+	})
+
+	// TODO close out and serialize the triples to a file...
+
+	return size, err
 }
 
 func visit(path string, f os.FileInfo, err error) error {
-
 	fileInfo := FileMetadata{}
 
 	if f.IsDir() {
@@ -54,7 +126,6 @@ func visit(path string, f os.FileInfo, err error) error {
 	// content via Tika
 	url := "http://localhost:9998/tika"
 	req, err := http.NewRequest("PUT", url, bytes.NewBuffer(data))
-	// req.Header.Set("X-Custom-Header", "myvalue")
 	req.Header.Set("Accept", "text/plain")
 
 	client := &http.Client{}
@@ -71,24 +142,15 @@ func visit(path string, f os.FileInfo, err error) error {
 	body, _ := ioutil.ReadAll(resp.Body)
 	cleanBody := stopwords.CleanString(string(body), "en", true)
 	fileInfo.LenContentNoStopWords = utf8.RuneCountInString(cleanBody)
-
-	fmt.Printf("For file: %s \nMD5:\t%v \nUUID:\t%s \nContentLen:\t%v \n\n", path, fileInfo.MD5, fileInfo.UUID, fileInfo.LenContentNoStopWords)
-
-	//     fmt.Printf("is not dir: %s\n", path)
-	// fmt.Printf("Found: %s  %d\n", f.Name(), f.Size())
-
-	// // get path elements
-	// dir, file := filepath.Split(path)
-	//     fmt.Printf("input: %q\n\tdir: %q\n\tfile: %q\n", path, dir, file)
+	dir, file := filepath.Split(path)
 
 	// // split so I can use a slice element in a lookup for metadata
 	// fmt.Printf("%q\n", strings.Split(path, "/"))
 
-	//  NEED the following SEVEN functions to be written...  all should be relatively easy...
-	//  Call to triple store a simple HTTP call with a teplated SPARQL query
-	// filemeta := GetFileMeta(path string, f os.FileInfo)  // Tika call  (do we need this?)
-	// x contentText := GetFileContent()  // Tike call
-	// projMeta := GetProjectMeta()  // SPARQL call
+	fmt.Printf("For path:\t%s \nFor dir:\t%s\nFor file:\t%s\nMD5:\t%v \nUUID:\t%s \nContentLen:\t%v \n\n", path, dir, file, fileInfo.MD5, fileInfo.UUID, fileInfo.LenContentNoStopWords)
+
+	//     fmt.Printf("is not dir: %s\n", path)
+	// fmt.Printf("Found: %s  %d\n", f.Name(), f.Size())
 
 	// indexJSON()  // pass to Bleve for indexing
 	// generateTriples() // build triples and append to a flat file for later use in triple store
